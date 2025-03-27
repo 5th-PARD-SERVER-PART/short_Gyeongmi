a39a7c3dbd4a9702261e87e265eecce677cd5d06
SeoGyeongmi <22200360@handong.ac.kr>
2025-03-27 16:38:11 +0900

    hoo..

---
+++ semina2/src/main/java/com/pard/semina2/Semina2Application.java
@@ -0,0 +1,13 @@
+package com.pard.semina2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semina2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semina2Application.class, args);
+    }
+
+}
---
+++ semina2/src/test/java/com/pard/semina2/Semina2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.semina2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semina2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
