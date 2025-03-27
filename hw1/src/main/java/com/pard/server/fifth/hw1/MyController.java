4f44c5562c90abecace407eccf10826b64d70b31
SeoGyeongmi <22200360@handong.ac.kr>
2025-03-27 16:21:03 +0900

    first init

---
+++ hw1/src/main/java/com/pard/server/fifth/hw1/MyController.java
@@ -0,0 +1,20 @@
+package com.pard.server.fifth.hw1;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class MyController {
+    @RequestMapping("")
+    public String hello(){
+        return "안뇽 ~";
+    }
+    @RequestMapping("/gm/{id}")
+    public String ID_and_TMI(@PathVariable String id,
+                             @RequestParam(required = false) String TMI){
+        if(TMI == null) {
+            TMI = "TMI 적으라우";
+        }
+        return "유저 ID : "+id+", TMI : "+TMI;
+    }
+}
