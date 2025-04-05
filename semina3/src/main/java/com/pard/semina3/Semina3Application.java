07bddeacaf1d36fd7f3036d82817317acd0a30c1
SeoGyeongmi <22200360@handong.ac.kr>
2025-04-05 15:10:28 +0900

    .

---
+++ semina3/src/main/java/com/pard/semina3/Semina3Application.java
@@ -0,0 +1,13 @@
+package com.pard.semina3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semina3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semina3Application.class, args);
+    }
+
+}
---
+++ semina3/src/test/java/com/pard/semina3/Semina3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.semina3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semina3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
