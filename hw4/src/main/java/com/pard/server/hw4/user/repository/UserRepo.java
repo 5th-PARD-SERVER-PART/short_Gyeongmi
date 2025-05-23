53082f54c9e48a5e64f1affb311e044e58a9aefc
kaila <130740278+SeoGyeongmi@users.noreply.github.com>
2025-05-24 06:54:43 +0900

    Delete hw4 directory

--- hw4/src/main/java/com/pard/server/hw4/user/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.pard.server.hw4.user.repository;
-
-import com.pard.server.hw4.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
