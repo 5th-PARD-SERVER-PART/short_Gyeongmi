7c0d402dbe66a729f3c093407133fe7eda2904d3
kaila <130740278+SeoGyeongmi@users.noreply.github.com>
2025-05-24 06:55:18 +0900

    Delete seminar4 directory

--- seminar4/src/main/java/com/pard/server/seminar4/book/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pard.server.seminar4.book.repository;
-
-import com.pard.server.seminar4.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
